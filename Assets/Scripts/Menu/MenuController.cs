930a84af363fc6e44109767af0ac45a1bb194f33
Joseph Antonio Carita <joseph.cmvd@gmail.com>
2025-09-05 16:53:05 -0500

    feat: se agrego disenio de menu principal y la base del primer nivel

---
+++ Assets/Scripts/Menu/MenuController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Exit...");
+        Application.Quit();
+    }
+}
